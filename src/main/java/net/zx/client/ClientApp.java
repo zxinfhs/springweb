0f2c43300e3e919ae5212f04e409465def9d934d
zxinfhs <zxinfhshotmail@gmail.com>
2015-05-10 17:43:21 +1000

    clean up code

---
+++ src/main/java/net/zx/client/ClientApp.java
@@ -0,0 +1,8 @@
+package net.zx.client;
+
+public class ClientApp {
+	public static void main(String[] args) {
+		System.out.println("spring mvc testing client");
+		
+	}
+}
--- src/main/java/net/zx/web/AppMain.java
+++
@@ -1,9 +0,0 @@
-package net.zx.web;
-
-public class AppMain {
-	
-	static public void main(String[] args) {
-		System.out.println("this is spring web app");
-	}
-
-}
